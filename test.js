7767ad048a4f8f7fd63da19d1e21a3aa9f8d777a
Carlos Filoteo <c.filoteo@live.com>
2018-09-28 00:25:27 -0600

    Add ava for testing

---
+++ test.js
@@ -0,0 +1,6 @@
+import test from 'ava'
+
+test.todo('test function that extracts data from review markup')
+test.todo('test function summarizes')
+test.todo('test function sorts review data')
+test.todo('test function that returns top accounts')
