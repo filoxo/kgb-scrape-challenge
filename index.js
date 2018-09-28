f460d7c5a7692752cf20a5246dbd9333d9b01180
Carlos Filoteo <c.filoteo@live.com>
2018-09-28 00:11:40 -0600

    Format using prettier

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
 #!/usr/bin/env node
 
-const rp = require('request-promise');
-const cheerio = require('cheerio'); 
+const rp = require('request-promise')
+const cheerio = require('cheerio')
 
 const { argv } = require('yargs')
-    .usage('kgb-scrape')
-    .options({
-      pages: {
-        alias: 'p',
-        default: 5,
-        describe: 'Number of pages to scrape',
-        type: 'number',
-        requiresArg: true,
-      },
-      top: {
-        alias: 't',
-        default: 3,
-        describe: 'Number of top results to return',
-        type: 'number',
-        requiresArg: true,
-      }
-    })
-    .help()
+  .usage('kgb-scrape')
+  .options({
+    pages: {
+      alias: 'p',
+      default: 5,
+      describe: 'Number of pages to scrape',
+      type: 'number',
+      requiresArg: true,
+    },
+    top: {
+      alias: 't',
+      default: 3,
+      describe: 'Number of top results to return',
+      type: 'number',
+      requiresArg: true,
+    },
+  })
+  .help()
 
 const options = {
-    uri: 'https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/?filter=ONLY_POSITIVE#link',
-    transform: (body) => cheerio.load(body)
-};
+  uri:
+    'https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/?filter=ONLY_POSITIVE#link',
+  transform: body => cheerio.load(body),
+}
 
 function main(argv) {
-    const {pages, top} = argv
-    const reviews = []
-    for(let i = 0; i < pages; i++) {
-        rp(options)
-            .then(function ($) {
-                const reviews = $('.review-entry')
-                console.log(reviews)
-                /** 
+  const { pages, top } = argv
+  const reviews = []
+  for (let i = 0; i < pages; i++) {
+    rp(options)
+      .then(function($) {
+        const reviews = $('.review-entry')
+        console.log(reviews)
+        /** 
                     CUSTOMER SERVICE	
                     QUALITY OF WORK	
                     FRIENDLINESS	
@@ -44,15 +45,15 @@ function main(argv) {
                     OVERALL EXPERIENCE	
                     RECOMMEND DEALER
                 **/
-                // Employees worked with, and their ratings
-                // other employees wored with
-                // was helpful count
-                // Date (most recent?)
-            })
-            .catch(function (err) {
-                // Crawling failed or Cheerio choked...
-            });
-    }
+        // Employees worked with, and their ratings
+        // other employees wored with
+        // was helpful count
+        // Date (most recent?)
+      })
+      .catch(function(err) {
+        // Crawling failed or Cheerio choked...
+      })
+  }
 }
 
-main(argv);
+main(argv)
